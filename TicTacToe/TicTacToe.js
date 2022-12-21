e831468c1b88f9a3bf9466048e28048712cd802d
jltlam <jlam011235@gmail.com>
2022-12-21 00:44:41 -0500

    hover! Colors!

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -7,9 +7,14 @@ class Game {
     // resets the board
     reset() {
         this.n = 3;
+        this.winner = [];
         this.moves = 0;
         this.user = 'X';
-        this.board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+        this.board = [];
+        for (let i = 0; i < this.n; i++) {
+            let row = new Array(this.n).fill('-', 0);
+            this.board.push(row);
+        }
         this.messages = "Game - START!";
         this.complete = false;
     }
@@ -17,7 +22,7 @@ class Game {
     // displays the board in text form
     // don't really need this, but it's good to see
     displayBoard() {
-        let text = this.board.join("\n").replaceAll(",", " ");
+        let text = this.board.map(row => row.join(" ")).join("\n");
         console.log(text);
         return text;
     };
@@ -48,6 +53,8 @@ class Game {
             // seems easier to do so, + consistency
             this.messages = `${this.user} is the winner!`;
             this.complete = true;
+            highlightWinner();
+            console.log(this.winner);
             return this.user;
         }
 
@@ -68,30 +75,55 @@ class Game {
         let win3 = true;
         let win4 = true;
 
+        
         // check row
         for (let i = 0; i < this.n; i++) {
+            this.winner.push(`${x}${i}`);
             if (this.board[x][i] !== this.user) {
                 win1 = false;
+                this.winner = [];
+                break;
             }
+        }
+        if (win1) {
+            return true;
         }
         // check col
         for (let i = 0; i < this.n; i++) {
+            this.winner.push(`${i}${y}`);
             if (this.board[i][y] !== this.user) {
                 win2 = false;
+                this.winner = [];
+                break;
             }
+        }
+        if (win2) {
+            return true;
         }
 
         // check diags
         for (let i = 0; i < this.n; i++) {
+            this.winner.push(`${i}${i}`);
             if (this.board[i][i] !== this.user) {
                 win3 = false;
+                this.winner = [];
+                break;
             }
+        }
+        if (win3) {
+            return true;
         }
 
         for (let i = 0; i < this.n; i++) {
+            this.winner.push(`${i}${this.n - 1 - i}`);
             if (this.board[i][this.n - 1 - i] !== this.user) {
                 win4 = false;
+                this.winner = [];
+                break;
             }
+        }
+        if (win4) {
+            return true;
         }
 
         return win1 || win2 || win3 || win4;
@@ -108,30 +140,45 @@ makeButtons();
 
 // making the buttons and setting them to the game
 function makeButtons() {
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            let button = document.getElementById(`${i}${j}`);
-            button.addEventListener('click', function () {
-                if (game.complete) {
-                    return;
-                }
-                buttons.push(button);
-
-                let m = game.makeMove(i, j);
-
-                button.textContent = m;
-                messages.textContent = "Messages: " + game.getMessages();
-                debug.textContent = game.displayBoard();
-                moves.textContent = "Move count: " + game.moves;
-            });
+    for (let i = 0; i < game.n; i++) {
+        for (let j = 0; j < game.n; j++) {
+            aButton(i, j);
         }
     }
+}
+
+function aButton(i, j) {
+    let button = document.getElementById(`${i}${j}`);
+    button.addEventListener('click', function () {
+        if (game.complete) {
+            return;
+        }
+        buttons.push(button);
+
+        let m = game.makeMove(i, j);
+
+        button.textContent = m;
+        messages.textContent = "Messages: " + game.getMessages();
+        debug.textContent = game.displayBoard();
+        moves.textContent = "Move count: " + game.moves;
+    });
+
+}
+
+function highlightWinner() {
+    game.winner.forEach(tile => {
+        document.getElementById(tile).classList.add("winner");
+    });
 }
 
 // all the html bits that are reset upon a new game
 function start() {
     console.log("New Game Started!");
     game.reset();
+    let tiles = document.getElementsByClassName("winner");
+    while (tiles.length > 0) {
+        tiles[0].classList.remove("winner");
+    }
     console.log(game.complete);
     document.getElementById("debug").textContent = game.displayBoard();
     moves.textContent = "Move count: " + game.moves;
