f192c8d0d37485831dfe7056899843bb4bf36ba8
jltlam <jlam011235@gmail.com>
2022-12-21 23:07:42 -0500

    some setup

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -1,107 +1,129 @@
 class Game {
-    moves = 0;
-    user = 'X';
-    board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
-    messages = "Game - START!";
-    complete = false;
 
+    constructor() {
+        this.reset();
+    }
+
+    // resets the board
     reset() {
+        this.n = 3;
+        this.winner = [];
         this.moves = 0;
         this.user = 'X';
-        this.board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+        this.board = [];
+        for (let i = 0; i < this.n; i++) {
+            let row = new Array(this.n).fill('-', 0);
+            this.board.push(row);
+        }
         this.messages = "Game - START!";
         this.complete = false;
     }
 
-    getMoves() {
-        return this.moves;
-    };
-
+    // displays the board in text form
+    // don't really need this, but it's good to see
     displayBoard() {
-        let text = '';
-        for (let i = 0; i < 3; i++) {
-            for (let k = 0; k < 3; k++) {
-                text += " " + this.board[i][k];
-            }
-            text += "\n";
-        }
+        let text = this.board.map(row => row.join(" ")).join("\n");
         console.log(text);
         return text;
     };
 
+    // returns any messages from the game
     getMessages() {
         return this.messages;
     }
 
+    // returns current user
     makeMove(x, y) {
         this.messages = "";
         if (this.complete) {
             this.messages = "GAME COMPLETED";
-            return;
+            return this.user;
         }
 
-        if (this.board[x][y] != '-') {
+        if (this.board[x][y] !== '-') {
             this.messages = "Invalid move! Try again!";
-            if (this.user == 'X') {
-                return 'O';
-            } else {
-                return 'X';
-            }
+            return this.user === 'X' ? 'O' : 'X';
         }
 
         this.board[x][y] = this.user;
         this.moves++;
 
         if (this.isWin(x, y)) {
-            this.messages = this.user + " is the winner!";
+            // string literal- why not just make every string like this then?
+            // seems easier to do so, + consistency
+            this.messages = `${this.user} is the winner!`;
             this.complete = true;
+            highlightWinner();
+            console.log(this.winner);
             return this.user;
         }
 
-        if (this.moves == 9) {
+        if (this.moves === 9) {
             this.messages = "It's a tie!";
             return this.user;
         }
 
-        if (this.user == 'X') {
-            this.user = 'O';
-            return 'X';
-        } else {
-            this.user = 'X';
-            return 'O';
-        }
+        let justWent = this.user;
+        this.user = this.user === 'X' ? 'O' : 'X';
+        return justWent;
     };
 
+    // checks if a move makes a user win
     isWin(x, y) {
         let win1 = true;
         let win2 = true;
         let win3 = true;
         let win4 = true;
 
+        
         // check row
-        for (let i = 0; i < 3; i++) {
-            if (this.board[x][i] != this.user) {
+        for (let i = 0; i < this.n; i++) {
+            this.winner.push(`${x}${i}`);
+            if (this.board[x][i] !== this.user) {
                 win1 = false;
+                this.winner = [];
+                break;
             }
         }
+        if (win1) {
+            return true;
+        }
         // check col
-        for (let i = 0; i < 3; i++) {
-            if (this.board[i][y] != this.user) {
+        for (let i = 0; i < this.n; i++) {
+            this.winner.push(`${i}${y}`);
+            if (this.board[i][y] !== this.user) {
                 win2 = false;
+                this.winner = [];
+                break;
             }
+        }
+        if (win2) {
+            return true;
         }
 
         // check diags
-        for (let i = 0; i < 3; i++) {
-            if (this.board[i][i] != this.user) {
+        for (let i = 0; i < this.n; i++) {
+            this.winner.push(`${i}${i}`);
+            if (this.board[i][i] !== this.user) {
                 win3 = false;
+                this.winner = [];
+                break;
             }
         }
+        if (win3) {
+            return true;
+        }
 
-        for (let i = 0; i < 3; i++) {
-            if (this.board[i][2 - i] != this.user) {
+        for (let i = 0; i < this.n; i++) {
+            this.winner.push(`${i}${this.n - 1 - i}`);
+            if (this.board[i][this.n - 1 - i] !== this.user) {
                 win4 = false;
+                this.winner = [];
+                break;
             }
+        }
+        if (win4) {
+            return true;
         }
 
         return win1 || win2 || win3 || win4;
@@ -109,49 +131,79 @@ class Game {
 
 }
 
+class ai {
+    constructor(board) {
+        this.board = board;
+    }
+
+
+    //p2 AI cannot be defeated muahahahahah
+}
+
+let twoPlayer = true;
 let game = new Game();
 let moves = document.getElementById("moves");
 let messages = document.getElementById("messages");
 let reset = document.getElementById("newGame");
-let buttons = new Array();
+let currPlayer = document.getElementById("currPlayer");
+let buttons = [];
 makeButtons();
-buttons.forEach(element => element.innerHTML = ".");
 
+// making the buttons and setting them to the game
 function makeButtons() {
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            let button = document.getElementById(i + "" + j);
-            button.addEventListener('click', function () {
-                if (game.complete) {
-                    return;
-                }
-                buttons.push(button);
-
-                let m = game.makeMove(i, j);
-
-                button.innerHTML = m;
-                messages.innerHTML = "Messages: " + game.getMessages();
-                debug.innerHTML = game.displayBoard();
-                moves.innerHTML = "Move count: " + game.getMoves();
-            });
+    for (let i = 0; i < game.n; i++) {
+        for (let j = 0; j < game.n; j++) {
+            aButton(i, j);
         }
     }
 }
 
+function aButton(i, j) {
+    let button = document.getElementById(`${i}${j}`);
+    button.addEventListener('click', function () {
+        if (game.complete) {
+            return;
+        }
+        buttons.push(button);
+
+        let m = game.makeMove(i, j);
+
+        button.classList.add("chosen");
+        button.textContent = m;
+        messages.textContent = "Messages: " + game.getMessages();
+        debug.textContent = game.displayBoard();
+        moves.textContent = "Move count: " + game.moves;
+        currPlayer.textContent = "Current Player: " + game.user;
+    });
+
+}
+
+function highlightWinner() {
+    game.winner.forEach(tile => {
+        document.getElementById(tile).classList.add("winner");
+    });
+}
+
+// all the html bits that are reset upon a new game
 function start() {
     console.log("New Game Started!");
     game.reset();
-    console.log(game.complete);
-    document.getElementById("debug").innerHTML = game.displayBoard();
-    moves.innerHTML = "Move count: " + game.getMoves();
-    messages.innerHTML = "Messages: " + game.getMessages();
-    buttons.forEach(element => element.innerHTML = ".");
+    let tiles = document.getElementsByClassName("winner");
+    while (tiles.length > 0) {
+        tiles[0].classList.remove("winner");
+    }
+    tiles = document.getElementsByClassName("chosen");
+    while (tiles.length > 0) {
+        tiles[0].classList.remove("chosen");
+    }
+    document.getElementById("debug").textContent = game.displayBoard();
+    moves.textContent = "Move count: " + game.moves;
+    currPlayer.textContent = "Current Player: X";
+    messages.textContent = "Messages: " + game.getMessages();
+    buttons.forEach(element => element.textContent = ".");
 }
 
-// why does this only work when I use "function()"?
-reset.addEventListener('click', function () {
-    start();
-});
+reset.addEventListener('click', start);
 
-
+// game... START!!!!
 start();
