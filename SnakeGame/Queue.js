75ab322f9f060bfcddd45b12c0fd6ec63283016a
jltlam <jlam011235@gmail.com>
2022-12-26 18:10:00 -0500

    basic snake movement, game and snake objects created

---
+++ SnakeGame/Queue.js
@@ -0,0 +1,36 @@
+var Queue = function () {
+    this.first = null;
+    this.size = 0;
+};
+
+var Node = function (data) {
+    this.data = data;
+    this.next = null;
+};
+
+Queue.prototype.enqueue = function (data) {
+    var node = new Node(data);
+
+    if (!this.first) {
+        this.first = node;
+    } else {
+        let n = this.first;
+        while (n.next) {
+            n = n.next;
+        }
+        n.next = node;
+    }
+
+    this.size += 1;
+    return node;
+};
+
+Queue.prototype.dequeue = function () {
+    let temp = this.first;
+    this.first = this.first.next;
+    this.size -= 1;
+    return temp;
+};
+
+
+//   https://stackoverflow.com/a/23573210/6622395
--- SnakeGame/Snake.js
+++ SnakeGame/Snake.js
@@ -1,9 +1,9 @@
 class Game {
-    constructor() {
-        this.reset();
+    constructor(snake) {
+        this.reset(snake);
     }
 
-    reset() {
+    reset(snake) {
         this.n = 7;
         this.board = [];
         for (let i = 0; i < this.n; i++) {
@@ -11,31 +11,70 @@ class Game {
             for (let j = 0; j < this.n; j++) {
                 row.push('-');
             }
-            this.board.push('-');
+            this.board.push(row);
         }
+        this.snake = snake;
+        this.gameState = "IN_PLAY";
     }
 
     debug() {
-        console.log(this.board);
+        console.log(this.board.join("\n"));
     }
 
-    tick(snake) {
-        this.board[snake.head[0]][snake.head[1]] = '$';
-        this.board[snake.tail[0]][snake.tail[1]] = '-';
+    end() {
+        console.log(this.snake.size);
+        this.gameState = "FINISHED";
+    }
+
+    tick(head, tail) {
+        console.log(head + " " + tail);
+        if (head[0] >= this.n || head[1] >= this.n) {
+            this.end();
+        }
+        this.board[head[0]][head[1]] = '$';
+        if (tail != null) {
+            this.board[tail[0]][tail[1]] = '-';
+        }
+        game.debug();
     }
 }
-
+ 
 class Snake {
-    constructor(game) {
+    constructor() {
         this.reset();
     }
 
     reset() {
         this.direction = "R";
-        // so I could do a queue-type of array, but this is fun too
-        this.head = [2, 0];
-        this.tail = [1, 0];
-        game.tick(this);
+        this.snake = new Queue();
+        this.head = [2, 3];
+        // start off as a 3 unit long snake
+        this.snake.enqueue([2, 1]);
+        this.snake.enqueue([2, 2]);
+        this.snake.enqueue([2, 3]);
+        game.tick(this.head, null);
+
+        this.size = 3;
+    }
+
+    move() {
+        if (this.direction == "R") {
+            this.snake.enqueue([this.head[0], this.head[1] + 1]);
+            this.head = [this.head[0], this.head[1] + 1];
+        } else if (this.direction == "U") {
+            this.snake.enqueue([this.head[0] - 1, this.head[1]]);
+            this.head = [this.head[0] - 1, this.head[1]];
+        } else if (this.direction == "L") {
+            this.snake.enqueue([this.head[0], this.head[1] - 1]);
+            this.head = [this.head[0], this.head[1] - 1];
+        } else if (this.direction == "D") {
+            this.snake.enqueue([this.head[0] + 1, this.head[1]]);
+            this.head = [this.head[0] + 1, this.head[1]];
+        }
+        let tail = this.snake.dequeue().data;
+        console.log(tail);
+
+        game.tick(this.head, tail);
     }
 }
 
@@ -43,19 +82,47 @@ class Snake {
 let board = document.getElementById("board");
 for (let i = 0; i < 7; i++) {
     board.innerHTML += "<div>";
+    let outerdiv = document.createElement("div");
+    outerdiv.classList.add("row");
     for (let j = 0; j < 7; j++) {
-        board.innerHTML += "<div class='tile'></div>";
+        let innerdiv = document.createElement("div");
+        innerdiv.classList.add("tile");
+        outerdiv.appendChild(innerdiv);
     }
-    board.innerHTML += "</div>";
+    board.appendChild(outerdiv);
 }
 let startButton = document.getElementById("newGame");
 startButton.addEventListener('click', start);
-
 let game = new Game();
 
 function start() {
-    let snake = new Snake(board);
-    console.log(game.board);
-
+    let snake = new Snake();
+    game.reset(snake);
+    window.addEventListener("keydown", (event) => {
+        switch (event.key) {
+            case "w":
+                if (snake.direction != "D") {
+                    snake.direction = "U";
+                }
+                break;
+            case "a":
+                if (snake.direction != "R") {
+                    snake.direction = "L";
+                }
+                break;
+            case "s":
+                if (snake.direction != "U") {
+                    snake.direction = "D";
+                } break;
+            case "d":
+                if (snake.direction != "L") {
+                    snake.direction = "R";
+                } break;
+            default:
+                console.log(event.key);
+                break;
+        }
+        snake.move();
+    });
 }
 
