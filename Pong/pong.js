c2e4f444b05ee1adf200f54fcd468e69ad7581fc
jltlam <jlam011235@gmail.com>
2023-01-17 16:44:33 -0500

    basic ball / bars movement

--- Pong/pong.js
+++ Pong/pong.js
@@ -18,10 +18,15 @@ const barSize = 80;
 // bars x coord is locked
 let ballx = 400;
 let bally = 200;
+let ballRad = 5; // ball radius
 const barlx = 20;
 let barly = 200;
 const barrx = 780;
 let barry = 200;
+
+// 1 is left, 2 is right
+let score1 = 0;
+let score2 = 0;
 
 // backdrop - constant
 function drawBackground() {
@@ -37,24 +42,17 @@ function drawBackground() {
   ctx.closePath();
 }
 
-function tick() {
-  if (canvas.getContext) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBackground();
-    ctx.strokeStyle = "#ffffff";
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(barlx, barly, 5, barSize);
-    ctx.fillRect(barrx, barry, 5, barSize);
-  }
-}
-
-tick();
-
-const keysPressed = new Map();
-// can use a map to hold all currently pressed keys instead
+const keysPressed = new Set();
 window.addEventListener("keydown", (event) => {
-  // gets snake's current direction
-  switch (event.key) {
+  keysPressed.add(event.key);
+});
+window.addEventListener("keyup", (event) => {
+  keysPressed.delete(event.key);
+});
+
+function checkKey(e) {
+  console.log(e);
+  switch (e) {
     case "ArrowUp":
       if (barry > 0) {
         barry -= barSpeed;
@@ -76,6 +74,74 @@ window.addEventListener("keydown", (event) => {
       }
       break;
   }
-  tick();
   console.log(barly + " " + barry);
-});
+}
+
+let xdir = 1;
+let ydir = 1;
+function tick() {
+  if (canvas.getContext) {
+    keysPressed.forEach(checkKey);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBackground();
+    ctx.strokeStyle = "#ffffff";
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(barlx, barly, 5, barSize);
+    ctx.fillRect(barrx, barry, 5, barSize);
+
+    // ball
+    ctx.beginPath();
+    ctx.arc(ballx, bally, ballRad, 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.closePath();
+
+    ballx += xdir;
+    bally += ydir;
+    console.log(canvas.height);
+
+    if (bally + ballRad >= canvas.height || bally - ballRad <= 0) {
+      ydir = -ydir;
+    }
+    if (ballx + ballRad >= canvas.width) { 
+      // left wins
+      goal(1);
+    } else if (ballx - ballRad <= 0) {
+      // right wins
+      goal(2);
+    }
+    if (hitsBar()) {
+      xdir = -xdir;
+    }
+
+  }
+  window.requestAnimationFrame(tick);
+}
+
+function goal(side) {
+  if (side == 1) {
+    score1++;
+  } else if (side == 2) {
+    score2++;
+  }
+  ballx = 400;
+  bally = 200;
+  document.getElementById("score").textContent = score1 + " " + score2;
+}
+
+function hitsBar() {
+  if (ballx - ballRad == barlx) {
+    if (bally > barly && bally < barly + barSize) {
+      return true;
+    }
+  } else if (ballx + ballRad == barrx) {
+    if (bally > barry && bally < barry + barSize) {
+      return true;
+    }
+  }
+  return false;
+}
+
+window.requestAnimationFrame(tick);
+
+
+// setInterval(tick, 40);
